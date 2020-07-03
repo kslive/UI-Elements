462b9de2fb8555cd42ecebdc6fc0926d5ea5d14f
kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-03 12:44:40 +0300

    UIPickerView. При выборе какого то ранее созданного элемента UIKit, все другие элементы скрываются с экрана, остается только выбранный элемент. Реализована кнопка Done в Toolbar, для скрытия PickerView после произведения выбора.

--- UI Elements/ViewController.swift
+++ UI Elements/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var uiElements = ["UISegmentedControl",
+                      "UILabel",
+                      "UISlider",
+                      "UITextField",
+                      "UIButton",
+                      "UIDatePicker"]
+    
+    var selectedElement: String?
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
@@ -35,6 +44,47 @@ class ViewController: UIViewController {
         slider.minimumTrackTintColor = .black
         slider.maximumTrackTintColor = .darkGray
         slider.thumbTintColor = .black
+        
+        choiceUIElement()
+        createToolbar()
+    }
+    
+    func hideAllElements() {
+        
+        segmentedControl.isHidden = true
+        label.isHidden = true
+        slider.isHidden = true
+        doneButton.isHidden = true
+        datePicker.isHidden = true
+    }
+    
+    func choiceUIElement() {
+        
+        let elementPicker = UIPickerView()
+        elementPicker.delegate = self
+    
+        textField.inputView = elementPicker
+    }
+    
+        
+    func createToolbar() {
+
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(dismissKeyboard))
+        
+        toolBar.setItems([doneButton], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
@@ -109,4 +159,46 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return uiElements.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return uiElements[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selectedElement = uiElements[row]
+        textField.text = selectedElement
+        
+        switch row {
+        case 0:
+            hideAllElements()
+            segmentedControl.isHidden = false
+        case 1:
+            hideAllElements()
+            label.isHidden = false
+        case 2:
+            hideAllElements()
+            slider.isHidden = false
+        case 3:
+            hideAllElements()
+        case 4:
+            hideAllElements()
+            doneButton.isHidden = false
+        case 5:
+            hideAllElements()
+            datePicker.isHidden = false
+        default:
+            hideAllElements()
+        }
+    }
+}
 
