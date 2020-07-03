fc5a933fc214e7ef212b3f4f3afcfdbf741903fd
kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-03 11:50:30 +0300

    UISwitch. При смене положения Switch все элементы на экране отображаются/скрываются.

--- UI Elements/ViewController.swift
+++ UI Elements/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var switchLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -87,6 +89,23 @@ class ViewController: UIViewController {
         dateFormatter.locale = Locale(identifier: "ru_RU")
         let dateValue = dateFormatter.string(from: sender.date  )
         label.text = dateValue
+    }
+    
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        
+        segmentedControl.isHidden = !segmentedControl.isHidden
+        label.isHidden = !label.isHidden
+        slider.isHidden = !slider.isHidden
+        textField.isHidden = !textField.isHidden
+        datePicker.isHidden = !datePicker.isHidden
+        doneButton.isHidden = !doneButton.isHidden
+        
+        if sender.isOn {
+            switchLabel.text = "Отобразить все элементы"
+        } else {
+            switchLabel.text = "Скрыть все элементы"
+        }
     }
 }
 
